fcf80d94098074a6fee581535e0c3d8f8292b2ec
lfb1206 <lucas.fernndez@uc.cl>
2022-05-18 11:57:19 -0400

    Avance

--- Sites/index.php
+++ Sites/index.php
@@ -6,14 +6,40 @@
 
   <br>
 
-  <h3 align="center"> ¿Quieres buscar un Pokemón por tipo y/o nombre?</h3>
+  <?php
+  #Primero obtenemos todos los tipos de pokemones
+  require("config/conexion.php");
+  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
 
-  <form align="center" action="consultas/consulta_tipo_nombre.php" method="post">
-    Tipo:
-    <input type="text" name="tipo_elegido">
+  <form align="center" action="consultas/consulta_pendiente.php" method="post">
+    Seleccinar un tipo:
+    <select name="tipo">
+      <?php
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    <br><br>
+    <input type="submit" value="Buscar por tipo">
+  </form>
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> ¿Quieres buscar un los vuelos aceptados por su aerolínea y el codigo ICAO del aeródromo?</h3>
+
+  <form align="center" action="consultas/consulta_aceptados.php" method="post">
+    Aerolínea:
+    <input type="text" name="aerolinea_escogida">
     <br/>
-    Nombre:
-    <input type="text" name="nombre_pokemon">
+    Codigo ICAO:
+    <input type="text" name="ICAO_elegido">
     <br/><br/>
     <input type="submit" value="Buscar">
   </form>
@@ -22,32 +48,18 @@
   <br>
   <br>
 
-  <h3 align="center"> ¿Quieres buscar un Pokemón por su ID?</h3>
+  <h3 align="center"> ¿Quieres conocer los tickets asociados a un codigo de reserva?</h3>
 
-  <form align="center" action="consultas/consulta_stats.php" method="post">
-    Id:
-    <input type="text" name="id_elegido">
+  <form align="center" action="consultas/consulta_reserva.php" method="post">
+    Codigo de reserva:
+    <input type="text" name="codigo_reserva">
     <br/><br/>
     <input type="submit" value="Buscar">
   </form>
-  
-  <br>
-  <br>
-  <br>
 
-  <h3 align="center"> ¿Quieres conocer los Pokemones más altos que: ?</h3>
-
-  <form align="center" action="consultas/consulta_altura.php" method="post">
-    Altura Mínima:
-    <input type="text" name="altura">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
   <br>
   <br>
   <br>
-
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
 
   <?php
   #Primero obtenemos todos los tipos de pokemones
@@ -57,7 +69,7 @@
   $dataCollected = $result -> fetchAll();
   ?>
 
-  <form align="center" action="consultas/consulta_tipo.php" method="post">
+  <form align="center" action="consultas/consulta_compra.php" method="post">
     Seleccinar un tipo:
     <select name="tipo">
       <?php
@@ -71,6 +83,41 @@
     <input type="submit" value="Buscar por tipo">
   </form>
 
+  <h3 align="center"> ¿Quieres conocer la cantidad de vuelos por estado asociados a una aerolínea?</h3>
+
+  <form align="center" action="consultas/consulta_.php" method="post">
+    Nombre aerolínea:
+    <input type="text" name="codigo_reserva">
+    <br/><br/>
+    <input type="submit" value="Buscar">
+  </form>
+  
+  <br>
+  <br>
+  <br>
+
+  <?php
+  #Primero obtenemos todos los tipos de pokemones
+  require("config/conexion.php");
+  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+
+  <form align="center" action="consultas/consulta_compra.php" method="post">
+    Seleccinar un tipo:
+    <select name="tipo">
+      <?php
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    <br><br>
+    <input type="submit" value="Buscar por tipo">
+  </form>
+  
   <br>
   <br>
   <br>
