3871ded5437090ff5d24778cebc236ba589d2fbf
negamartin <martin.andrighetti@gmail.com>
2022-05-19 21:30:50 -0400

    entregar icao

--- Sites/consulta_aceptados.php
+++ Sites/consulta_aceptados.php
@@ -9,12 +9,13 @@
   $codigo = $_POST["codigo"];
   $codigo = strtoupper($codigo);
 
-  $query = "SELECT Vuelo.numero_vuelo, Origen.nombre as origen, Destino.nombre as destino,
+  $query = "SELECT Vuelo.numero_vuelo, Origen.codigo_icao, Origen.nombre as origen,
+              Destino.codigo_icao, Destino.nombre as destino,
               Vuelo.fecha_salida, Vuelo.fecha_llegada, Vuelo.estado
             FROM Vuelo, CompaniaAerea, Aerodromo as Origen, Aerodromo as Destino
             WHERE UPPER(CompaniaAerea.nombre_aerolinea) LIKE '%$aerolinea_escogida%'
               AND CompaniaAerea.codigo_aerolinea = Vuelo.codigo_aerolinea 
-              AND UPPER(Vuelo.destino_icao) LIKE '%$codigo%' 
+              AND UPPER(Vuelo.destino_icao) LIKE '%$codigo%'
               AND Vuelo.estado = 'aceptado'
               AND Vuelo.origen_icao = Origen.codigo_icao
               AND Vuelo.destino_icao = Destino.codigo_icao;";
@@ -39,7 +40,16 @@
       </tr>
     <?php
     foreach ($dataCollected as $data) {
-        echo "<tr><td>$data[0]</td><td>$data[1]</td><td>$data[2]</td><td>$data[3]</td><td>$data[4]</td></tr>";
+        echo "<tr>
+          <td>$data[0]</td>
+          <td>$data[1]</td>
+          <td>$data[2]</td>
+          <td>$data[3]</td>
+          <td>$data[4]</td>
+          <td>$data[5]</td>
+          <td>$data[6]</td>
+          <td>$data[7]</td>
+        </tr>";
     }
     ?>
     </table>
