2d7acd267b3846ca52f4fd6a95a8a3b252a8060e
lfb1206 <lucas.fernndez@uc.cl>
2022-06-20 18:06:04 -0400

    .

--- Sites/templates/header.php
+++ Sites/templates/header.php
@@ -4,8 +4,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./styles/mystyles.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+        <link rel="stylesheet" href="./styles/mystyles.css">
         <title> Plataforma de gestión comercial de vuelos </title>
     </head>
 
