964e6d4319157085e09f2fc684e5ceb3d6a493e6
lfb1206 <lucas.fernndez@uc.cl>
2022-06-20 16:01:42 -0400

    .

--- Sites/login.php
+++ Sites/login.php
@@ -8,13 +8,13 @@ if ($request_method  === 'POST') {
   // Se está recibiendo datos para el login
   session_start();
   // Aquí se tendría que buscar el id del usuario en la BDD con el mail y la contraseña
-  $user_id = 1;
-  $user_name = "Ash";
+  $user_name = $_POST['username'];
+  $password = $_POST['password'];
 
   // Se guardan estos valores en la sesión
   // falta que de alguna parte salgan el user_id y User_name que están hardcodeados arriba
-  $_SESSION['user_id'] = $user_id;
   $_SESSION['user_name'] = $user_name;
+  $_SESSION['password'] = $password;
 
   // Mandamos al usuario al inicio
   go_home();
@@ -29,11 +29,17 @@ if ($request_method  === 'POST') {
     <div class="columns is-mobile is-centered is-vcentered cover-all">
       <div class="column is-half">
         <!-- https://bulma.io/documentation/form/general/ -->
-        <form method="POST">
+        <form method="post">
           <div class="field">
             <label class="label">Nombre de usuario</label>
             <div class="control">
-              <input class="input" type="text" name="username" value="Ash">
+              <input class="input" type="text" name="username">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Contraseña</label>
+            <div class="control">
+              <input class="input" type="text" name="password">
             </div>
           </div>
           <button class="button is-primary" type="submit" name="login">Login</button>
@@ -43,3 +49,4 @@ if ($request_method  === 'POST') {
   </section>
 <?php include './templates/footer.html';
 } ?>
+
---
+++ Sites/sesion_aerolineas.php
@@ -0,0 +1,71 @@
+<?php include('templates/header.html');   ?>
+
+<body>
+  <?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("config/conexion.php");
+
+  $aerolinea_escogida = $_POST["aerolinea_escogida"];
+
+  $query = "SELECT Vuelo.numero_vuelo, Origen.codigo_icao, Origen.nombre as origen,
+              Destino.codigo_icao, Destino.nombre as destino,
+              Vuelo.fecha_salida, Vuelo.fecha_llegada, Vuelo.estado
+            FROM Vuelo, CompaniaAerea, Aerodromo as Origen, Aerodromo as Destino
+            WHERE UPPER(CompaniaAerea.nombre_aerolinea) LIKE '%$aerolinea_escogida%'
+              AND CompaniaAerea.codigo_aerolinea = Vuelo.codigo_aerolinea 
+              AND Vuelo.estado = 'aceptado'
+              AND Vuelo.origen_icao = Origen.codigo_icao
+              AND Vuelo.destino_icao = Destino.codigo_icao;";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+
+  $query2 = "SELECT Vuelo.numero_vuelo, Origen.codigo_icao, Origen.nombre as origen,
+              Destino.codigo_icao, Destino.nombre as destino,
+              Vuelo.fecha_salida, Vuelo.fecha_llegada, Vuelo.estado
+            FROM Vuelo, CompaniaAerea, Aerodromo as Origen, Aerodromo as Destino
+            WHERE UPPER(CompaniaAerea.nombre_aerolinea) LIKE '%$aerolinea_escogida%'
+              AND CompaniaAerea.codigo_aerolinea = Vuelo.codigo_aerolinea 
+              AND Vuelo.estado = 'rechazado'
+              AND Vuelo.origen_icao = Origen.codigo_icao
+              AND Vuelo.destino_icao = Destino.codigo_icao;";
+  $result2 = $db -> prepare($query);
+  $result2 -> execute();
+  $dataCollected2 = $result2 -> fetchAll();
+  ?>
+
+  <?php
+  echo "<h1 align=\"center\">Vuelos rechazados para aerolínea \"$aerolinea_escogida\" y con destino \"$codigo\"</h1>";
+  ?>
+
+  <div class="surface">
+
+    <table>
+      <tr>
+        <th>Código de vuelo</th>
+        <th>ICAO origen</th>
+        <th>Aeródromo origen</th>
+        <th>ICAO destino</th>
+        <th>Aeródromo destino</th>
+        <th>Fecha salida</th>
+        <th>Fecha llegada</th>
+        <th>Estado</th>
+      </tr>
+    <?php
+    foreach ($dataCollected as $data) {
+        echo "<tr>
+          <td>$data[0]</td>
+          <td>$data[1]</td>
+          <td>$data[2]</td>
+          <td>$data[3]</td>
+          <td>$data[4]</td>
+          <td>$data[5]</td>
+          <td>$data[6]</td>
+          <td>$data[7]</td>
+        </tr>";
+    }
+    ?>
+    </table>
+  </div>
+
+<?php include('templates/footer.html'); ?>
