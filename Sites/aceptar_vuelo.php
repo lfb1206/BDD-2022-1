e54223fad29074bca121628ce69ebe8f99f2f4a7
lfb1206 <lucas.fernndez@uc.cl>
2022-06-20 18:14:32 -0400

    .

--- Sites/aceptar_vuelo.php
+++ Sites/aceptar_vuelo.php
@@ -3,6 +3,15 @@
 $vuelo = $_GET['vuelo']
 ?>
 <h2 class="title">Aceptar vuelo <?php echo "$vuelo" ?></h2>
+<?php
+    require("config/conexion.php");
+    $query = "UPDATE vuelo 
+              SET estado = 'aceptado' 
+              WHERE id_vuelo = $id_vuelo";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $vuelos = $result -> fetchAll();
+?>
 
 <div class="buttons">
     <a class="button is-info is-rounded is-outlined is-right" href="sesion_admin.php">
