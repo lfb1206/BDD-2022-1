5f7c5fa0429e7385122de2981b917862eb59782a
lfb1206 <lucas.fernndez@uc.cl>
2022-06-20 17:43:18 -0400

    .

--- Sites/aceptar_vuelo.php
+++ Sites/aceptar_vuelo.php
@@ -1,12 +1,6 @@
 <?php include('templates/header.php');   ?>
-<h2 class="title">Aceptar vuelo id_vuelo</h2>
+<h2 class="title">Aceptar vuelo <?php $id_vuelo ?></h2>
 
-<?php
-    require("config/conexion.php");
-    $query = "UPDATE vuelo SET  ";
-    $result = $db -> prepare($query);
-    $result -> execute();
-    $vuelos = $result -> fetchAll();
-?>
+
 
 <?php include('templates/footer.php'); ?>
