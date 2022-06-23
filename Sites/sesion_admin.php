262ddc5f4919986da94e0308bde240a75f8866bf
lfb1206 <lucas.fernndez@uc.cl>
2022-06-23 13:11:19 -0400

    .

--- Sites/sesion_admin.php
+++ Sites/sesion_admin.php
@@ -24,33 +24,35 @@ if (isset($_POST["fecha_minima"]) and isset($_POST["fecha_maxima"])) {
 }
 
 ?>
-<table>
-  <tr>
-    <th>ID</th>
-    <th>codigo</th>
-    <th>Aceptar</th>
-    <th>Rechazar</th>
-  </tr>
-<?php
-foreach ($vuelos as $vuelo) {
-    ?>
-    <tr> 
-      <td><?php echo "$vuelo[0]"; ?></td> 
-      <td><?php echo "$vuelo[1]"; ?></td> 
-      <td><?php
-        echo "<a href=\"aceptar_vuelo.php?vuelo=$vuelo[0]\"> Aceptar </a>"
-      ?></td>
-      <td><?php
-          echo "<a href=\"rechazar_vuelo.php?vuelo=$vuelo[0]\"> Rechazar </a>"
-      ?></td>
+<div class="column is-4 is-centered is-vcentered">
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>codigo</th>
+      <th>Aceptar</th>
+      <th>Rechazar</th>
     </tr>
     <?php
-}
-?>
-</table> 
+    foreach ($vuelos as $vuelo) {
+        ?>
+        <tr> 
+          <td><?php echo "$vuelo[0]"; ?></td> 
+          <td><?php echo "$vuelo[1]"; ?></td> 
+          <td><?php
+            echo "<a href=\"aceptar_vuelo.php?vuelo=$vuelo[0]\"> Aceptar </a>"
+          ?></td>
+          <td><?php
+              echo "<a href=\"rechazar_vuelo.php?vuelo=$vuelo[0]\"> Rechazar </a>"
+          ?></td>
+        </tr>
+        <?php
+    }
+    ?>
+  </table> 
+  </div>
 
   <div class="columns is-mobile is-centered is-vcentered cover-all">
-    <div class="column is-4 is-offset-4">
+    <div class="column is-4">
       <!-- https://bulma.io/documentation/form/general/ -->
       <form action="sesion_admin.php" method="post">
         <div class="field">
