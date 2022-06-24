72ff76e36541cf63c324a10a2ba87698539951ac
lfb1206 <lucas.fernndez@uc.cl>
2022-06-24 18:51:47 -0400

    .

--- Sites/aceptar_vuelo.php
+++ Sites/aceptar_vuelo.php
@@ -14,27 +14,43 @@ $vuelo = $_GET['vuelo'];
 ?>
 
 <?php
-/*
-    $query = "SELECT *
+
+    $query2 = "SELECT *
               FROM vuelo 
               WHERE id_vuelo = $vuelo";
+    $result2 = $db2 -> prepare($query2);
+    $result2 -> execute();
+    $vuelos2 = $result2 -> fetchAll();
+
+    $query3 = "SELECT codigo_ICAO
+              FROM Codigo_aerodromo, Aerodromo
+              WHERE Aerodromo.aerodromo_id = $vuelos2[5]
+              AND Aerodromo.id_codigos = Codigo_aerodromo";
+    $result3 = $db2 -> prepare($query3);
+    $result3 -> execute();
+    $icao3 = $result3 -> fetchAll();
+
+    $query4 = "SELECT codigo_ICAO
+              FROM Codigo_aerodromo, Aerodromo
+              WHERE Aerodromo.aerodromo_id = $vuelos2[6]
+              AND Aerodromo.id_codigos = Codigo_aerodromo";
+    $result4 = $db2 -> prepare($query4);
+    $result4 -> execute();
+    $icao4 = $result4 -> fetchAll();
+
+?>
+<?php
+$velocidad = rand(200, 300);
+$altitud = rand(10000, 13000);
+$id_ruta = rand(1, 200)
+?>
+
+<?php
+    $query = "INSERT INTO Vuelo($vuelos2[0], $icao3[0], $icao4[0],$vuelos2[9],$vuelos2[2], $vuelos2[3], $velocidad,  $altitud,  $id_ruta,  $vuelos2[5], 'aceptado') ";
     $result = $db -> prepare($query);
     $result -> execute();
     $vuelos = $result -> fetchAll();
 ?>
-
-<?php
-    $query = "SELECT *
-              FROM dblink
-              ('dbname=grupo42e3
-               port=5432
-               password=grupo42
-               user=grupo42', INSERT TO 
-              );
-    $result = $db -> prepare($query);
-    $result -> execute();
-    $vuelos = $result -> fetchAll();
-*/?>
 
 
 <div class="buttons" style="justify-content: center;">
