9f0009369c5b28ff41455b3eb9104a2f09633081
lfb1206 <lucas.fernndez@uc.cl>
2022-06-23 23:49:32 -0400

    .

--- Sites/index.php
+++ Sites/index.php
@@ -19,7 +19,7 @@
       $result -> execute();
       $dataCollected = $result -> fetchAll();
       ?>
-      <h2 class="title is-1"> Hola <?php echo "$dataCollected"; ?> </h2>
+      <h2 class="title is-1"> Hola <?php echo "$dataCollected[0]"; ?> </h2>
       <a class="button is-link" href="sesion_aerolineas.php">Proponer vuelos</a>
       <a class="button is-link" href="sesion_aerolineas_aceptado.php">Vuelos aceptados</a>
       <a class="button is-link" href="sesion_aerolineas_rechazado.php">Vuelos rechazado</a> 
@@ -32,7 +32,7 @@
       $result -> execute();
       $dataCollected = $result -> fetchAll();
       ?>
-      <h2 class="title is-1"> Hola <?php echo "$dataCollected"; ?> </h2>
+      <h2 class="title is-1"> Hola <?php echo "$dataCollected[0]"; ?> </h2>
       <a class="button is-link" href="sesion_pasajeros.php">Reservar</a>
       <a class="button is-link" href="datos_pasajero.php">Datos</a> 
       <?php
