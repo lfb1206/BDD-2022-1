0a4c70d9cba0bcd7418c171cccee715dfcaf5da8
lfb1206 <lucas.fernndez@uc.cl>
2022-06-24 18:00:33 -0400

    .

--- Sites/logout.php
+++ Sites/logout.php
@@ -1,5 +1,22 @@
 <?php
-include 'templates/header.php';
+require_once __DIR__ . "/../config/data.php";
+
+// Se conecta a la BDD
+require_once __DIR__ . "/../config/conexion.php";
+
+// Se crea siempre una sesión
+session_start();
+
+
+// Funciones propias de utilidad
+
+/**
+ * Volver al inicio, tiene que se llamada antes de entregar HTML.
+ */
+function go_home() {
+  header("Location: " . '/~grupo19/');
+  exit();
+}
 session_destroy();
 go_home();
 ?>
