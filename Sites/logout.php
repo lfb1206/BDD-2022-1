2fbad0d40425a797de50a7da0086d13100613a04
lfb1206 <lucas.fernndez@uc.cl>
2022-06-24 18:01:03 -0400

    .

--- Sites/logout.php
+++ Sites/logout.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . "/../config/data.php";
+require_once __DIR__ . "/config/data.php";
 
 // Se conecta a la BDD
-require_once __DIR__ . "/../config/conexion.php";
+require_once __DIR__ . "/config/conexion.php";
 
 // Se crea siempre una sesión
 session_start();
